5dd673f04cf5a095eb974574346a51fb1067808e
Thiago Sampaio <160647048+ThiagoGSampaio@users.noreply.github.com>
2024-02-27 15:24:10 -0300

    aula 27-02

---
+++ componets/exemplo_0/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function exemplo_0(){
+    return(
+        <View>
+            <Text>Minha primeira tela</Text>
+        </View>
+    );
+}
+
+export default exemplo_0;
