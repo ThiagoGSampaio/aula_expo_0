e1e16d325edda171414c01d7ab52ca1b9966c596
Thiago Sampaio <160647048+ThiagoGSampaio@users.noreply.github.com>
2024-03-12 15:23:26 -0300

    Aula 12-03

---
+++ componets/atividade_1/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    
+    container:{
+        backgroundColor:'orange',
+        padding:10,
+    },
+
+    texto:{
+        color: 'black',
+        fontSize:20,
+    },
+
+    texto2:{
+        color: 'white',
+        fontSize:20,
+    },
+
+});
+
+export default styles;
--- componets/exemplo_1/index.js
+++ componets/exemplo_1/index.js
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native';
-import styles from './style';
+import styles from './styles';
 
 function Exemplo_0(){
     return(
