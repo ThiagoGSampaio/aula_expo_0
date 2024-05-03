768aeab5cd2cc69f45d28a0bb6f014371b6ccd16
Thiago Sampaio <160647048+ThiagoGSampaio@users.noreply.github.com>
2024-05-03 09:45:32 -0300

    Aula 03-05

--- componets/exemplo_3/index.js
+++ componets/exemplo_3/index.js
@@ -5,8 +5,8 @@ export default function Exemplo_3 () {
     return(
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 3</Text>
-            <Pressable>
-                <Text>Ex. botão</Text>
+            <Pressable onPress={() => alert('Auto Desturição em 10s')} style={styles.botao}>
+                <Text >Clique Aqui</Text>
             </Pressable>
         </View>
     )
--- componets/exemplo_3/styles.js
+++ componets/exemplo_3/styles.js
@@ -5,14 +5,28 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#D1C4E9',
         borderRadius:20,
+        width: '100%',
+        alignItems: 'center',
     },
 
     titulo:{
         fontSize: 30,
         fontWeight: 'bold',
         marginVertical: 20,
-        color: '#673AB7'
-    }
+        color: '#673AB7',
+        
+    },
+
+    botao:{
+        backgroundColor: "#fff",
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 2,
+        width: '80%',
+        alignItems: 'center'
+    },
+
+ 
 })
 
 export default styles;
